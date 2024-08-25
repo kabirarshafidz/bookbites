e3f1a7649027933e1b73229fb7553daee4763f03
kabirarshafidz <hafidzarsha@gmail.com>
2024-08-26 02:04:32 +0700

    revert

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import pg from "pg";
 import "dotenv/config";
-import pool from "./database.js";
 
 const app = express();
 const port = 3000;
@@ -15,11 +14,21 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT * FROM books ORDER BY id ASC");
+  const result = await db.query("SELECT * FROM books ORDER BY id ASC");
   const books = result.rows;
   console.log(books);
   res.render("index.ejs", { books: books });
@@ -41,7 +50,7 @@ app.get("/new", (req, res) => {
 });
 
 app.get("/edit/:id", async (req, res) => {
-  const result = await pool.query("SELECT * FROM books ORDER BY id ASC");
+  const result = await db.query("SELECT * FROM books ORDER BY id ASC");
   const existingBooks = result.rows;
   const id = parseInt(req.params.id);
   const bookToEdit = existingBooks.find((book) => book.id === id);
@@ -61,7 +70,7 @@ app.post("/new", async (req, res) => {
   const note = req.body.note;
 
   try {
-    await pool.query(
+    await db.query(
       "INSERT INTO books (isbn, title, author, date, rating, note) VALUES ($1, $2, $3, $4, $5, $6)",
       [isbn, title, author, date, rating, note]
     );
@@ -75,7 +84,7 @@ app.post("/new", async (req, res) => {
 
 app.post("/edit/:id", async (req, res) => {
   const id = parseInt(req.params.id);
-  const result = await pool.query("SELECT * FROM books WHERE id = ($1)", [id]);
+  const result = await db.query("SELECT * FROM books WHERE id = ($1)", [id]);
   const bookToEdit = result.rows[0];
   const editedIsbn = parseInt(req.body.isbn) || bookToEdit.isbn;
   const editedTitle = req.body.title || bookToEdit.title;
@@ -84,7 +93,7 @@ app.post("/edit/:id", async (req, res) => {
   const editedRating = parseFloat(req.body.rating) || bookToEdit.rating;
   const editedNote = req.body.note || bookToEdit.note;
   try {
-    await pool.query(
+    await db.query(
       "UPDATE books SET isbn = $1, title = $2, author = $3, date = $4, rating = $5, note = $6 WHERE id = $7",
       [
         editedIsbn,
@@ -106,7 +115,7 @@ app.post("/edit/:id", async (req, res) => {
 app.post("/delete/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   try {
-    await pool.query("DELETE FROM books WHERE id = ($1)", [id]);
+    await db.query("DELETE FROM books WHERE id = ($1)", [id]);
     console.log("Deleted!");
     res.redirect("/");
   } catch (error) {
